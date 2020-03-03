9b978d38d604c76ed3a6ce351fb5c24b7fc25526
James Lenahan <james.k.lenahan@gmail.com>
2020-03-03 19:54:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: {minimum: 100}
-  validates :category, acceptance: true, message: ("Fiction")
-  validates :category, acceptance: true, message: ("Non-Fiction")
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :content, length: { minimum: 100 }
 end
